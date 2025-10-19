14025f122eb84a8728f85e49f98fa47ad223a7d6
Vaishnavi Pawar <vaishnavi.22211188@viit.ac.in>
2025-10-19 20:13:32 +0530

    implement company form ui

--- frontend/src/components/Company/Company.jsx
+++ frontend/src/components/Company/Company.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Loader2, Edit3 } from "lucide-react";
+import { Loader2, Pencil, ChevronRight } from "lucide-react";
 
 function Company() {
   const [company, setCompany] = useState(null);
@@ -53,86 +53,143 @@ function Company() {
     <div className="pt-28 pl-72 pr-6 pb-10 bg-gray-100 min-h-screen">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-semibold text-gray-800">Company Profile</h1>
-        <button
-          className="flex items-center gap-2 bg-indigo-600 text-white px-4 py-2 rounded-lg shadow hover:bg-indigo-700 transition"
-          title="Edit Company Info"
-        >
-          <Edit3 className="w-4 h-4" />
-          Edit
-        </button>
       </div>
 
-      <div className="bg-white shadow-md rounded-2xl p-8 border border-gray-200 max-w-4xl">
+      <div className="bg-white shadow-md rounded-2xl py-8 px-4 border border-gray-200 max-w-6xl">
         {/* Company Info Section */}
         <div className="mb-8">
-          <h2 className="text-xl font-semibold text-gray-700 mb-4 border-b pb-2">
-            Company Information
-          </h2>
+          <div className="flex flex-row items-center justify-between border-b border-gray-300 pb-2 mb-4">
+            <h2 className="text-xl ml-2 font-semibold text-gray-700">
+              Company Information
+            </h2>
+          </div>
+
+          {/* Information rows */}
           <div className="space-y-4">
-            <InfoRow label="Name" value={company.company_name} />
-            <InfoRow label="Email" value={company.company_email} />
-            <InfoRow label="Plan" value={company.plan} />
-            <InfoRow
-              label="Status"
-              value={
-                company.isVerified ? (
+            <div className="flex justify-between items-center border-b border-gray-100 pb-2">
+              <span className="text-gray-500 font-normal ml-5">Name</span>
+              <span className="text-gray-800 font-normal">
+                {company.company_name}
+              </span>
+              <button className="cursor-pointer">
+                <ChevronRight className="w-4 h-4 text-purple-700 mr-8" />
+              </button>
+            </div>
+
+            <div className="flex justify-between items-center border-b border-gray-100 pb-2">
+              <span className="text-gray-500 font-normal ml-5">Email</span>
+              <span className="text-gray-800 font-normal">
+                {company.company_email}
+              </span>
+              <button className="cursor-pointer">
+                <ChevronRight className="w-4 h-4 text-purple-700 mr-8" />
+              </button>
+            </div>
+
+            <div className="flex justify-between items-center border-b border-gray-100 pb-2">
+              <span className="text-gray-500 font-normal ml-5">Plan</span>
+              <span className="text-gray-800 font-normal mr-25">
+                {company.plan}
+              </span>
+              <button className="cursor-pointer">
+                <ChevronRight className="w-4 h-4 text-purple-700 mr-8" />
+              </button>
+            </div>
+
+            <div className="flex justify-between items-center border-b border-gray-100 pb-2">
+              <span className="text-gray-500 font-normal ml-5">Status</span>
+              <span className="text-gray-800 font-normal mr-21">
+                {company.isVerified ? (
                   <span className="text-green-600 font-medium">Verified</span>
                 ) : (
                   <span className="text-yellow-600 font-medium">
                     Not Verified
                   </span>
-                )
-              }
-            />
+                )}
+              </span>
+              <button className="cursor-pointer">
+                <ChevronRight className="w-4 h-4 text-purple-700 mr-8" />
+              </button>
+            </div>
           </div>
         </div>
 
         {/* Team Info Section */}
         <div className="mb-8">
-          <h2 className="text-xl font-semibold text-gray-700 mb-4 border-b pb-2">
-            Team Information
-          </h2>
+          <div className="flex flex-row items-center justify-between border-b border-gray-300 pb-2 mb-4">
+            <h2 className="text-xl ml-2 font-semibold text-gray-700">
+              Team Information
+            </h2>
+          </div>
+
           <div className="space-y-4">
-            <InfoRow label="Admins" value={company.no_of_admin} />
-            <InfoRow label="Staff" value={company.no_of_staff} />
+            <div className="flex justify-between items-center border-b border-gray-100 pb-2">
+              <span className="text-gray-500 font-normal ml-5">Admins</span>
+              <span className="text-gray-800 font-normal mr-25">
+                {company.no_of_admin}
+              </span>
+              <button className="cursor-pointer">
+                <ChevronRight className="w-4 h-4 text-purple-700 mr-8" />
+              </button>
+            </div>
+
+            <div className="flex justify-between items-center border-b border-gray-100 pb-2">
+              <span className="text-gray-500 font-normal ml-5">Staff</span>
+              <span className="text-gray-800 font-normal mr-19">
+                {company.no_of_staff}
+              </span>
+              <button className="cursor-pointer">
+                <ChevronRight className="w-4 h-4 text-purple-700 mr-8" />
+              </button>
+            </div>
           </div>
         </div>
 
         {/* Address & Timezone Section */}
         <div className="mb-8">
-          <h2 className="text-xl font-semibold text-gray-700 mb-4 border-b pb-2">
-            Address & Timezone
-          </h2>
+          <div className="flex flex-row items-center justify-between border-b border-gray-300 pb-2 mb-4">
+            <h2 className="text-xl ml-2 font-semibold text-gray-700">
+              Address & Timezone
+            </h2>
+          </div>
+
           <div className="flex flex-col gap-4">
-            <InfoRow label="Address" value={company.address || "N/A"} />
-            <InfoRow
-              label="Timezone"
-              value={
-                company.timezoneFrom && company.timezoneTo
+            <div className="flex justify-between items-center border-b border-gray-100 pb-2">
+              <span className="text-gray-500 font-normal ml-5">Address</span>
+              <span className="text-gray-800 font-normal mr-20">
+                {company.address || "N/A"}
+              </span>
+              <button className="cursor-pointer">
+                <ChevronRight className="w-4 h-4 text-purple-700 mr-8" />
+              </button>
+            </div>
+
+            <div className="flex justify-between items-center border-b border-gray-100 pb-2">
+              <span className="text-gray-500 font-normal ml-5">Timezone</span>
+              <span className="text-gray-800 font-normal mr-28">
+                {company.timezoneFrom && company.timezoneTo
                   ? `${company.timezoneFrom} - ${company.timezoneTo}`
-                  : "N/A"
-              }
-            />
+                  : "N/A"}
+              </span>
+              <button className="cursor-pointer">
+                <ChevronRight className="w-4 h-4 text-purple-700 mr-8" />
+              </button>
+            </div>
           </div>
         </div>
 
         {/* Footer */}
-        <div className="border-t pt-4 text-xs text-gray-500 flex justify-between">
+        {/* <div className="border-t pt-4 text-xs text-gray-500 ml-2 flex justify-between">
           <span>
-            Created at: {new Date(company.created_at).toLocaleString()}
+            Created at:{" "}
+            {new Date(company.created_at).toLocaleString("en-IN", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+            })}
           </span>
-        </div>
+        </div> */}
       </div>
-    </div>
-  );
-}
-
-// Reusable row component
-function InfoRow({ label, value }) {
-  return (
-    <div className="flex justify-between items-center border-b border-gray-100 pb-2">
-      <span className="text-gray-600 font-medium">{label}</span>
-      <span className="text-gray-800 font-semibold">{value}</span>
     </div>
   );
 }
